746513103a4b9febc2d66a0057c601108c5ebadf
kaushal <kaushalpandey455@gmail.com>
2022-12-06 21:57:54 +0530

    add html page for web page

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ regmodel=pickle.load(open('regmodel.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 @app.route('/predict_api',methods=['POST'])
@@ -24,6 +24,13 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("index.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
