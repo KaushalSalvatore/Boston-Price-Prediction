485c5e5278a58ebd299c52ff64b5ddb9c16283b7
kaushal <kaushalpandey455@gmail.com>
2023-06-20 16:06:12 +0530

    add some file

--- index.py
+++ index.py
@@ -34,6 +34,5 @@ def predict():
     return render_template("index.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__=="__main__":
-    app.run()
-    
-    # app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    # app.run()
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
